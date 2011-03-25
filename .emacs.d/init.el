dbc9fe8722e899f593a5cea88e159dcd97b9209f
Charles Pence <charles@charlespence.net>
2011-03-25 15:37:15 -0400

    [emacs] More general settings.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,9 +8,35 @@
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 
-;; Disable splash screen, file backups
+;; ----------------------------------------------------
+;; Emacs GUI
+
+;; Disable splash screen
 (setq inhibit-splash-screen t)
+
+;; Disable menubar if in the terminal
+(if (not window-system) (menu-bar-mode -1) (menu-bar-mode 1))
+
+;; Disable scroll bar, tool bar
+(scroll-bar-mode nil)
+(tool-bar-mode nil)
+
+;; Line and column numbers
+(line-number-mode t)
+(column-number-mode t)
+
+;; ----------------------------------------------------
+;; File saving
+
+;; Don't make backups
 (setq make-backup-files nil)
+
+;; Deal correctly with newlines
+(setq next-line-add-newlines nil)
+(setq require-final-newline t)
+
+;; ----------------------------------------------------
+;; Fonts and Colors
 
 ;; Load color theme
 (require 'color-theme-autoloads "color-theme-autoloads")
@@ -18,3 +44,17 @@
 (load-file "~/.emacs.d/site-lisp/color-theme/themes/color-theme-railscasts.el")
 (color-theme-railscasts)
 
+;; Enable maximum font-lock goodness
+(if window-system
+    (progn
+      (setq font-lock-maximum-decoration t)
+      (add-hook 'after-init-hook
+		(global-font-lock-mode t))))
+
+;; Set font
+(set-face-font 'default "Panic Sans-14:antialias=subpixel")
+
+;; ----------------------------------------------------
+;; Keys
+
+;; 
