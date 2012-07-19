33e020b65dd5e380a6ff28f16836c6276878aa50
Charles Pence <charles@charlespence.net>
2012-07-19 08:45:20 -0500

    [emacs] Remove python-mode, use built-in instead.

--- .emacs.d/init.d/modeline.el
+++ .emacs.d/init.d/modeline.el
@@ -35,10 +35,7 @@
     ("jabber-chat" . "jc")
     ("jabber-roster" . "jr")
     ("Ruby" . "rb")
-    ("mu4e.main" . "m4m")
-    ("mu4e.headers" . "m4h")
-    ("mu4e.view" . "m4v")
-    ("mu4e.compose" . "m4c")))
+    ("Python" . "py")))
 
 (defun cp-get-short-mode-name ()
   (or
