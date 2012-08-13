9e25ace4c693e3878659acf5e7691759c59c5a02
Charles Pence <charles@charlespence.net>
2012-08-13 13:41:58 -0400

    [emacs] Try a new buffer filter to not pick up output buffers.

--- .emacs.d/init.d/ido.el
+++ .emacs.d/init.d/ido.el
@@ -13,7 +13,7 @@
       ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
       ido-ignore-buffers '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
                            "^\*compilation" "^\*GTAGS" "^session\.*" "^\*Minibuf"
-                           "^\*mu4e-proc" "^\*Echo" "output*$")
+                           "^\*mu4e-proc" "^\*Echo" ".*[oO]utput\*$")
       ido-ignore-files (append ido-ignore-files
                                (list
                                 ".*\\.org_archive$"
