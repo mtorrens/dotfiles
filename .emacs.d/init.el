9fd431bda83e9e48fd30b9373ef4c8ba26867bce
Charles Pence <charles@charlespence.net>
2013-08-09 12:39:51 -0400

    [Emacs] Emacs is back, baby.

---
+++ .emacs.d/init.el
@@ -0,0 +1,135 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(setq el-get-sources
+    '((:name base16-themes
+             :description "Base16 provides carefully chosen syntax highlighting and a default set of sixteen colors suitable for a wide range of applications."
+             :website "http://chriskempson.github.io/base16/"
+             :type github
+             :pkgname "milkypostman/base16-emacs"
+             :post-init (add-to-list 'custom-theme-load-path
+                                     default-directory))
+))
+
+(setq my:el-get-packages
+      '(base16-themes
+	magit
+	ruby-mode
+	rspec-mode
+	haml-mode
+	sass-mode
+	scss-mode
+	markdown-mode
+	icomplete+
+	fill-column-indicator))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync my:el-get-packages)
+
+
+;; Fonts and visuals
+(set-default-font "Droid Sans Mono:pixelsize=14")
+(setq-default line-spacing 2)
+
+(add-to-list 'default-frame-alist '(width . 115))
+(add-to-list 'default-frame-alist '(height . 35))
+
+(load-theme 'base16-railscasts-dark t)
+(tool-bar-mode -1)
+(global-linum-mode 1)
+(setq linum-format "%6d ")
+(global-hl-line-mode t)
+(show-paren-mode t)
+(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+
+;; Don't change the format of this next line; Emacs looks for it.
+(setq inhibit-startup-echo-area-message
+      "cpence")
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      fringe-mode '(1 . 0)
+      search-highlight t
+      query-replace-highlight t
+      show-paren-style 'parenthesis)
+
+;; Keyboard settings
+(cua-mode t)
+(delete-selection-mode 1)
+(setq x-select-enable-clipboard t
+      interprogram-paste-function 'x-cut-buffer-or-selection-value)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Scrolling
+(setq scroll-margin 0
+      scroll-conservatively 10000
+      scroll-up-aggressively 0
+      scroll-down-aggressively 0
+      scroll-preserve-screen-position t)
+
+;; File saving
+(setq-default fill-column 80)
+(setq make-backup-files nil
+      require-final-newline t
+      locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(set-input-method nil)
+(setq read-quoted-char-radix 10)
+
+;; Modeline
+(line-number-mode t)
+(column-number-mode t)
+
+;; Minibuffer
+(icomplete-mode t)
+(setq icomplete-prospects-height 1
+      icomplete-compute-delay 0)
+(require 'icomplete+ nil noerror)
+
+;; Buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward
+      uniquify-separator ":"
+      uniquify-after-kill-buffer-p t
+      uniquify-ignore-buffers-re "^\\*")
+
+;; Recent and history
+(setq savehist-autosave-interval 60
+      savehist-file "~/.emacs.d/cache/savehist")
+(savehist-mode t)
+(require 'recentf)
+(setq recentf-save-file "~/.emacs.d/cache/recentf"
+      recentf-max-saved-items 100
+      recentf-max-menu-items 15)
+(recentf-mode t)
+(setq auto-save-list-file-prefix "~/.emacs.d/cache/auto-save-list/.saves-")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Keyboard
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+;; Ruby/RSpec
+(setq rspec-use-rake-when-possible nil
+      rspec-use-bundler-when-possible t)
