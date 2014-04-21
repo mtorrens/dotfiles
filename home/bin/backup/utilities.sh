265be6c70e8b63d59aef68a0d7f225dccb9d6102
Charles Pence <charles@charlespence.net>
2014-04-21 12:05:01 -0400

    [bin] Reenable encryption on backup private dir files.

--- home/bin/backup/utilities.sh
+++ home/bin/backup/utilities.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-# Get and remove GPG-encrypted private data files, doing our best 
+# Get and remove GPG-encrypted private data files, doing our best
 # to trap signals and not leave anything orphaned in ${HOME}.
 
-GPG=/usr/local/bin/gpg
+GPG=/usr/bin/gpg
+
+# Encrypt files with:
+# gpg --encrypt -r charles@charlespence.net <file>
+
+# Manually decrypt with
+# gpg --output <out> --decrypt <file>.gpg
 
 function get_private_file {
-	
+
 	# Decrypt the file with a safe umask
 	umask 077 && $GPG --output ${HOME}/$2 --decrypt ${HOME}/.private/$1.gpg
-	
+
 	# Delete the file if the script fails
 	trap "rm -f ${HOME}/$2; exit" INT TERM EXIT
 }
 
 function release_private_file {
-	
+
 	# Delete the file
 	rm ${HOME}/$1
-	
+
 	# Release our trap
 	trap - INT TERM EXIT
 }
