c2a546a97b596a1154676432734dc2aee0c11f83
Charles Pence <charles@charlespence.net>
2014-07-11 11:07:46 -0400

    [Conky] Update scripts to be a top-line status bar.

--- home/.conky_lua_scripts.lua
+++ home/.conky_lua_scripts.lua
@@ -1,19 +1,41 @@
-function conky_pad2( number )
-    return string.format( '%2u' , conky_parse( number ) )
+function parse_num(str)
+  result = conky_parse(str)
+  if result == nil then return nil end
+
+  return tonumber(result)
 end
 
-function conky_pad3( number )
-    return string.format( '%3u' , conky_parse( number ) )
+function conky_pad2(str)
+  num = parse_num(str)
+  if num == nil then return '' end
+
+  return string.format('%2u', num)
 end
 
-function conky_pad31( number )
-    return string.format( '%5.1f' , conky_parse( number ) )
+function conky_pad3(str)
+  num = parse_num(str)
+  if num == nil then return '' end
+
+  return string.format('%3u', num)
 end
 
-function conky_pad4( number )
-    return string.format( '%4u' , conky_parse( number ) )
+function conky_pad31(str)
+  num = parse_num(str)
+  if num == nil then return end
+
+  return string.format('%5.1f', num)
 end
 
-function conky_pad41( number )
-    return string.format( '%6.1f' , conky_parse( number ) )
+function conky_pad4(str)
+  num = parse_num(str)
+  if num == nil then return '' end
+
+  return string.format('%4u', num)
+end
+
+function conky_pad41(str)
+  num = parse_num(str)
+  if num == nil then return '' end
+
+  return string.format('%6.1f', num)
 end
