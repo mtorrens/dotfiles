17740f077cfa812c40adf1090ffbafc58feedeca
Charles Pence <charles@charlespence.net>
2015-01-25 20:39:40 -0600

    Don't hardcode the GPG path.

--- home/bin/backup/utilities.sh
+++ home/bin/backup/utilities.sh
@@ -2,8 +2,6 @@
 # Get and remove GPG-encrypted private data files, doing our best
 # to trap signals and not leave anything orphaned in ${HOME}.
 
-GPG=/usr/bin/gpg
-
 # Encrypt files with:
 # gpg --encrypt -r charles@charlespence.net <file>
 
@@ -13,7 +11,7 @@ GPG=/usr/bin/gpg
 function get_private_file {
 
 	# Decrypt the file with a safe umask
-	umask 077 && $GPG --output ${HOME}/$2 --decrypt ${HOME}/.private/$1.gpg
+	umask 077 && gpg --output ${HOME}/$2 --decrypt ${HOME}/.private/$1.gpg
 
 	# Delete the file if the script fails
 	trap "rm -f ${HOME}/$2; exit" INT TERM EXIT
