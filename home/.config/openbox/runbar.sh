96bcf4a0856c37b5de0103ffbfb0cdcefbc548f9
Charles Pence <charles@charlespence.net>
2017-08-19 11:04:22 -0500

    [OB] Another shot at fixing stalonetray.

--- home/.config/openbox/runbar.sh
+++ home/.config/openbox/runbar.sh
@@ -4,14 +4,16 @@
 monline=`xrandr -q | grep '+0+0'`
 monx=`echo $monline | sed "s@.* \([0-9]\+\)x[0-9]\+.*@\1@"`
 
-$HOME/.config/openbox/bar.sh | lemonbar -p -g ${monx}x18+0+0 \
+barwidth=$((monx - 320))
+
+$HOME/.config/openbox/bar.sh | lemonbar -p -g ${barwidth}x16+0+0 \
     -F '#d3d0c8' -B '#2d2d2d' \
-    -f '-gohu-gohufont-medium-r-normal--14-*-*-*-*-*-*-*' \
-    -f '-gohu-gohufont-bold-r-normal--14-*-*-*-*-*-*-*' \
-    -f "Font Awesome" | bash &
+    -o -1 -f '-gohu-gohufont-medium-r-normal--14-*-*-*-*-*-*-*' \
+    -o -1 -f '-gohu-gohufont-bold-r-normal--14-*-*-*-*-*-*-*' \
+    -o 1 -f "Font Awesome" | bash &
 
 sleep 1
 
 trayx=$((monx - 320))
 
-stalonetray --geometry 20x1+${trayx}+0 --sticky true &
+stalonetray --geometry 20x1+${trayx}+0 --sticky true --kludges force_icons_size,fix_window_pos &
