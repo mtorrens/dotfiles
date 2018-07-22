ced7bff247e646c497e62e9ad6c1195280765fac
Charles Pence <charles@charlespence.net>
2018-07-22 14:59:45 -0500

    [bar] Stop using lemonbar.

--- home/.config/bar/screenon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Make sure the screen stays on
-
-# Disable monitor power saving modes
-xset -dpms
-
-# Start a process that will keep the locker from activating
-light-locker-command -i &
